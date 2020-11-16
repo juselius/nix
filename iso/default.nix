ff50ca95d4c7e0fbbef9e54261c7c9e20985f305
Jonas Juselius <jonas.juselius@itpartner.no>
2020-11-16 10:44:05 +0100

    More UEFI tweaks

--- iso/default.nix
+++ iso/default.nix
@@ -2,8 +2,16 @@
 # $ nix-build make-iso.nix
 
 let
+## TOS Hyper-V
+  # dev = "/dev/sda";
+  # user = "admin";
+  # eth = "eth0";
+  # boot = "uefi";
+## Dora VMWare
   dev = "/dev/sda";
   user = "admin";
+  eth = "ens32";
+  boot = "bios";
   initialPassword = "en to tre";
   config = (import <nixpkgs/nixos/lib/eval-config.nix> {
     system = "x86_64-linux";
@@ -15,16 +23,22 @@ let
           name = "nixos-configuration";
           src = ./nixos;
           buildCommand = ''
-          mkdir -p $out
-          cp -r $src/* $out
+            mkdir -p $out
+            cp -r $src/* $out
+            sed -i "s,bootdisk = .*,bootdisk= \"${dev}\";," $out/options.nix
+            if [ "${boot}" = "uefi" ]; then
+              sed -i "s,uefi = .*,uefi = true;," $out/options.nix
+            else
+              sed -i "s,uefi = .*,uefi = false;," $out/options.nix
+            fi
           '';
         };
         dotfiles = pkgs.stdenv.mkDerivation {
           name = "dotfiles";
           src = ./dotfiles;
           buildCommand = ''
-          mkdir -p $out
-          cp -r $src/* $out
+            mkdir -p $out
+            cp -r $src/* $out
           '';
         };
       in
@@ -33,9 +47,9 @@ let
         services.openssh.permitRootLogin = "yes";
         users.extraUsers.root.initialPassword = lib.mkForce "${initialPassword}";
         environment.systemPackages = with pkgs; [
-        git
+          git wget
         ];
-        networking.extraHosts = "10.253.18.100 iplogger.k0.local";
+        networking.extraHosts = "10.253.18.100 logger.kube0.local";
         systemd.services.inception = {
         description = "Self-bootstrap a NixOS installation";
         wantedBy = [ "multi-user.target" ];
@@ -44,19 +58,26 @@ let
         script = with pkgs; ''
           set +e
 
-          parted -s ${dev} -- mklabel gpt
-          parted -s ${dev} -- mkpart primary 512MiB -4GiB
-          parted -s ${dev} -- mkpart primary linux-swap -4GiB 100%
-          parted -s ${dev} -- mkpart ESP fat32 1MiB 512MiB
-          parted -s ${dev} -- set 3 boot on
-
-          mkfs.ext4 -F -L nixos ${dev}1
-          mkswap -L swap ${dev}2
-          mkfs.fat -F 32 -n boot ${dev}3
-
-          mount ${dev}1 /mnt
-          mkdir -p /mnt/boot
-          mount ${dev}3 /mnt/boot
+          if [ ${boot} = "uefi" ]; then
+            parted -s ${dev} -- mklabel gpt
+            parted -s ${dev} -- mkpart primary 512MiB -4GiB
+            parted -s ${dev} -- mkpart primary linux-swap -4GiB 100%
+            parted -s ${dev} -- mkpart ESP fat32 1MiB 512MiB
+            parted -s ${dev} -- set 3 boot on
+            mkfs.ext4 -F -L nixos ${dev}1
+            mount ${dev}1 /mnt
+            mkswap -L swap ${dev}2
+            mkfs.fat -F 32 -n boot ${dev}3
+            mkdir -p /mnt/boot
+            mount ${dev}3 /mnt/boot
+          else
+            parted -s ${dev} -- mklabel msdos
+            parted -s ${dev} -- mkpart primary 1MiB -4GiB
+            parted -s ${dev} -- mkpart primary linux-swap -4GiB 100%
+            mkfs.ext4 -F -L nixos ${dev}1
+            mount ${dev}1 /mnt
+            mkswap -L swap ${dev}2
+          fi
 
           mkdir -p /mnt/etc/nixos/
           cp -r ${nixos-configuration}/* /mnt/etc/nixos
@@ -68,17 +89,18 @@ let
             ${config.system.build.nixos-install}/bin/nixos-install \
             > /tmp/nixos-install
           chmod 755 /tmp/nixos-install
+
+          sleep 30 # wait for dhcp before install
           /tmp/nixos-install --no-root-passwd -j 1
 
-          echo ${initialPassword} | passwd ${user}
+          cp -r ${dotfiles} /mnt/home/${user}/.dotfiles
 
-          cp -r ${dotfiles} /home/${user}/.dotfiles
+          ip=`ip a s dev ${eth} |sed -n 's/ *inet \+\(\([0-9]\+\.\?\)\{4\}\).*/\1/p'`
+          wget "http://collector.kube2.local/api/log?entry=$ip" && true
 
-          ip=`ip a s dev eno2 |sed -n 's/ *inet \+\(\([0-9]\+\.\?\)\{4\}\).*/\1/p'`
-          wget "http://iplogger.k0.local/api/log?entry=$ip" && true
-
-          # eject -r -s -F /dev/sr0 && true
-          # ${systemd}/bin/shutdown -r now
+          chroot /mnt echo ${initialPassword} | passwd ${user}
+          eject -r -s -F /dev/sr0 && true
+          ${systemd}/bin/shutdown -r now
         '';
         environment = config.nix.envVars // {
           inherit (config.environment.sessionVariables) NIX_PATH;
