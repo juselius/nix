ea0966538129c9e6b415037d2a322d9297df377d
Jonas Juselius <jonas.juselius@itpartner.no>
2024-05-02 15:45:21 +0200

    add mft nixos module

---
+++ mft/module.nix
@@ -0,0 +1,29 @@
+{config, lib, pkgs, ...}:
+with lib;
+let
+  kernel = config.boot.kernelPackages.kernel;
+
+  mft = pkgs.callPackage ./mft.nix { inherit kernel; };
+in
+{
+  ###### interface
+
+  options.features.hpc.mft = {
+    enable = mkEnableOption "Mellanox MFT";
+  };
+
+  ###### implementation
+
+  config = mkIf config.features.hpc.mft.enable {
+    environment.etc."mft/mft.conf".source = "${mft.mft}/etc/mft/mft.conf";
+    environment.etc."mft/mst.conf".source = "${mft.mft}/etc/mft/mst.conf";
+    environment.etc."mft/ca-bundle.crt".source = "${mft.mft}/etc/mft/ca-bundle.crt";
+
+    environment.systemPackages = [ mft.mft ];
+
+    # boot = {
+    #   kernelModules = [ "mst_pci" "mst_pciconf" ];
+    #   extraModulePackages = [ mft.mft-kernel-module ];
+    # };
+  };
+}
